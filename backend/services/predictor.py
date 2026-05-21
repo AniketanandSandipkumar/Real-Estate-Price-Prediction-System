539f1ca71a793e2fb838925139f385f8b8850e71
AniketanandSandipkumar <119923008+AniketanandSandipkumar@users.noreply.github.com>
2026-05-21 11:31:59 +0530

    Update predictor.py

--- backend/services/predictor.py
+++ backend/services/predictor.py
@@ -3,18 +3,18 @@ import pandas as pd
 
 from ml_pipeline.feature_engineering import create_features
 
-
 model = joblib.load(
     "backend/models/production_model.pkl"
 )
 
-
 def predict_price(data):
 
-    df = pd.DataFrame([data])
+    input_data = data.model_dump()
+
+    df = pd.DataFrame([input_data])
 
     df = create_features(df)
 
     prediction = model.predict(df)[0]
 
-    return round(prediction, 2)
+    return round(float(prediction), 2)
